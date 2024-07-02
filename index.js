964ec7bae6daebd0e937d573a148419fd9fd5277
Arsalan Khattak <me@khattak.dev>
2024-07-02 18:11:25 +0500

    missing files

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+const neo4j = require("neo4j-driver");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+const URI = process.env.NEO4J_URI;
+const USER = process.env.NEO4J_USERNAME;
+const PASSWORD = process.env.NEO4J_PASSWORD;
+const DBNAME = process.env.NEO4J_DBNAME || "neo4j";
+
+app.get("/load", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/sync", (req, res) => {
   res.send("Hello World!");
 });
 
